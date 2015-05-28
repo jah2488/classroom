2a9b43d7015d6404fbe8c0f911f5528d222527ff
Justin Herrick <justin@theironyard.com>
2015-05-27 22:33:27 -0500

    Adding quick 'mark as complete' button to instructor dash.

---
+++ app/assets/javascripts/components/mark_as_complete.js.jsx
@@ -0,0 +1,34 @@
+/* global React, jQuery */
+var MarkAsComplete = React.createClass({
+    getInitialState: function () {
+        return {
+            sent: false
+        };
+    },
+    handleClick: function () {
+        jQuery.ajax({
+            method: 'PATCH',
+            url: '/submissions/' + this.props.submissionID + '/' + 'complete'
+        }).done(function (response) {
+            this.setState({ sent: true });
+        }.bind(this));
+    },
+    render: function () {
+        if (this.state.sent) {
+            return (
+                <div>
+                    <div className="alert alert-success alert-dismissible" role="alert">
+                        <button type="button" className="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        Complete
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <button onClick={this.handleClick} type="button" className="btn btn-xs btn-default" aria-label="Correct">
+                  <span className="glyphicon glyphicon-ok" aria-hidden="true"></span>
+                </button>
+            );
+        }
+    }
+});
--- app/controllers/instructor_dashboard_controller.rb
+++ app/controllers/instructor_dashboard_controller.rb
@@ -1,29 +1,35 @@
 class InstructorDashboardController < ApplicationController
   def index
-    cohort        = current_instructor.current_cohort
-    if cohort
-      students    = cohort.students
-      assignments = cohort.assignments.order('due_date ASC')
-      submissions = Submission.ungraded_for(cohort)
-      #get submissions for current_cohort
-      # only return ungraded submissions sorted by assignment due date
-      render locals: {
-        instructor: current_instructor,
-        cohort: cohort,
-        current_day: Day.current_for(cohort),
-        assignments: assignments,
-        submissions: submissions,
-        students: students
-      }
+    if cohort = current_instructor.current_cohort
+      render_with_cohort(cohort)
     else
-      render locals: {
-        instructor: current_instructor,
-        cohort: Cohort.new(name: "no cohort"),
-        current_day: nil,
-        assignments: [],
-        submissions: [],
-        students: []
-      }
+      render_without_cohort
     end
+  end
+
+  def render_with_cohort(cohort)
+    students    = cohort.students
+    assignments = cohort.assignments.order('due_date DESC')
+    submissions = Submission.ungraded_for(cohort) #get submissions for current_cohort
+    # only return ungraded submissions sorted by assignment due date
+    render locals: {
+      instructor: current_instructor,
+      cohort: cohort,
+      current_day: Day.current_for(cohort),
+      assignments: assignments,
+      submissions: submissions,
+      students: students
+    }
+  end
+
+  def render_without_cohort
+    render locals: {
+      instructor: current_instructor,
+      cohort: Cohort.new(name: "no cohort"),
+      current_day: nil,
+      assignments: [],
+      submissions: [],
+      students: []
+    }
   end
 end
