807825aea041d6fe9a04dab8c1678154a4747c58
Justin Herrick <justin@theironyard.com>
2015-08-01 18:33:25 -0500

    Adding cohort feature.

---
+++ spec/features/staff_cohorts_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+RSpec.feature "StaffCohorts", type: :feature do
+  let!(:instructor) do
+    create :instructor_user, email: 'instructor@theironyard.com', password: 'password', name: 'Jane Doe'
+  end
+
+  feature 'Picking Cohort' do
+    scenario 'with multiple available cohorts' do
+      create :cohort, name: 'Design Summer', instructor: instructor.instructor
+      rails_cohort = create :cohort, name: 'Rails Summer', instructor: instructor.instructor
+
+      sign_in(:instructor)
+
+      find(:css, "a[data-id='#{rails_cohort.id}']").click
+
+      expect(page).to have_content('Dashboard')
+      expect(page).to have_content('Reports')
+      expect(page).to have_content('New Assignment')
+      expect(page).to have_content('New Badge')
+    end
+
+    scenario 'creating a cohort' do
+      create :campus, name: 'Moon'
+
+      sign_in(:instructor)
+      click_new_cohort_link
+      create_cohort
+      expect_cohort_to_be_created
+    end
+    #Notes about this particular test/scenario:
+    #  - Thought I'd play with the idea of making the scenario as readable as possible
+    #    so when it does break, you're changing the method its using, not the scenario itself.
+    #    Could help isolate especially brittle areas of the test or the code.
+    #  - I realize that this tests is taking the big assumption that you will be redirected to the staff_cohorts_index after sign_in
+
+    def sign_in(type)
+      case type
+      when :instructor
+        visit new_user_session_path
+        fill_in 'Email', with: 'instructor@theironyard.com'
+        fill_in 'Password', with: 'password'
+        click_button 'Log in'
+        expect(page).to have_content('Signed in successfully')
+      else
+        raise 'Sign in type not found. Please add a new entry for that type.'
+      end
+    end
+
+    def click_new_cohort_link
+      find(:css, "a[href='#{new_staff_cohort_path}']").click
+    end
+
+    def create_cohort
+      fill_in 'Name', with: 'Rails'
+      select 'Moon', from: 'Campus'
+      select 'Jane Doe', from: 'Instructor'
+
+      click_button 'Create Cohort'
+    end
+
+    def expect_cohort_to_be_created
+      expect(page).to have_content('New Cohort successfully created!')
+    end
+  end
+end
---
+++ spec/features/staff_dashes_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature "StaffDashes", type: :feature do
+  feature 'The Cohort Dashboard' do
+    scenario 'has assignments, students, susbmissions, and adjustments' do
+    end
+
+    scenario 'viewing assignments' do
+    end
+    scenario 'creating assignments' do
+    end
+
+    scenario 'viewing submissions' do
+    end
+    scenario 'viewing assignments' do
+    end
+
+  end
+end
