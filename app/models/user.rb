a6b1c61de2562fcdf6380d22863b1fd1b0465889
Kurt Nelson <kurtisnelson@gmail.com>
2015-08-05 16:12:37 -0500

    Use gem for gravatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
+  include Gravtastic
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_one :instructor
   has_one :student
+  has_gravatar
 
   def instructor?
     !instructor.nil?
