26a8ff36e55fc945484f92904ba260de42f7a84b
Kurt Nelson <kurtisnelson@gmail.com>
2015-08-08 15:15:35 -0500

    Show instructor avatars on cohort list

--- app/decorators/instructor_decorator.rb
+++ app/decorators/instructor_decorator.rb
@@ -1,4 +1,10 @@
 class InstructorDecorator < Draper::Decorator
   delegate_all
-
+  def avatar_url(size = 80)
+    if object.user
+      object.user.decorate.avatar_url(size)
+    else
+      "https://secure.gravatar.com/avatar/44e0021f61946ce33b0c965356f6ff99.png?r=PG&s=#{size}"
+    end
+  end
 end
