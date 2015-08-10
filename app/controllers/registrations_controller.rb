ed4403ce56c2cbcb719c549e9af931f96fc6b0e3
Kurt Nelson <kurtisnelson@gmail.com>
2015-08-10 14:53:35 -0500

    Show msg about confirm after sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -14,5 +14,11 @@ class RegistrationsController < Devise::RegistrationsController
   def update
     super
   end
+
+  private
+
+  def after_inactive_sign_up_path_for(resource)
+    new_user_session_path
+  end
 end
 
