9b9b128682932a564f833aa8d356ea531ef60006
Kurt Nelson <kurtisnelson@gmail.com>
2015-08-18 16:51:07 -0500

    Remove unused gems

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,18 +1,3 @@
-Rails.application.config.middleware.use ExceptionNotification::Rack, {
-  :email => {
-    :email_prefix => "[CLASSROOM ERROR] ",
-    :sender_address => %{"notifier" <notifier@tiy-classroom.herokuapp.com>},
-    :exception_recipients => %w{justin@theironyard.com}
-  },
-  :slack => {
-    :webhook_url => "https://hooks.slack.com/services/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",
-    :additional_parameters => {
-      :icon_emoji => ":alarm_clock:",
-      :mrkdwn => true
-    }
-  }
-}
-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { :host => 'tiy-classroom.herokuapp.com' }
