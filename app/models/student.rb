4861ed6325ba9120f2dfa0f8e2dcddb19b7f08e2
Justin Herrick <justin@justinherrick.com>
2015-08-23 21:33:24 -0500

    No need to offset absences.

--- app/models/student.rb
+++ app/models/student.rb
@@ -24,7 +24,7 @@ class Student < ActiveRecord::Base
   end
 
   def absences
-    (cohort.days.count - checkins.count) - 1 #offset first day
+    cohort.days.count - checkins.count
   end
 
   def completed_assignments
