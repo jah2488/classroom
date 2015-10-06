d34dff32f5ff1fdf1061cdf7b114005123e2778e
Kurt Nelson <kurtisnelson@gmail.com>
2015-10-06 15:37:28 -0500

    Update Refile

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,5 +1,5 @@
 require "refile/s3"
-Refile.configure do |config|
+if Rails.env.production?
   aws = {
     access_key_id: ENV['AWS_ID'],
     secret_access_key: ENV['AWS_SECRET'],
@@ -8,5 +8,9 @@ Refile.configure do |config|
   }
   Refile.cache = Refile::S3.new(prefix: "cache", **aws)
   Refile.store = Refile::S3.new(prefix: "store", **aws)
-  Refile.cdn_host = "//" + (ENV['ASSET_HOST'] || 'example.com') unless Rails.env.test?
+  Refile.cdn_host = "//" + ENV['ASSET_HOST']
+else
+  backend = Refile::Backend::FileSystem.new("tmp")
+  Refile.cache = backend
+  Refile.store = backend
 end
