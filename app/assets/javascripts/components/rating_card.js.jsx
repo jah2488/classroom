c9b0306f9e5380e9dcd9fec7c939c5ca15c427e7
Kurt Nelson <kurtisnelson@gmail.com>
2015-10-16 10:36:52 -0500

    Ratings can be edited

--- app/assets/javascripts/components/rating_card.js.jsx
+++ app/assets/javascripts/components/rating_card.js.jsx
@@ -1,16 +1,65 @@
 var RatingCard = React.createClass({
+        getInitialState: function () {
+                return {
+                        rating: this.props.rating,
+                        editing: false
+                };
+        },
+        update: function () {
+                jQuery.ajax({
+                        method: 'PATCH',
+                        url: '/ratings/' + this.state.rating.id,
+                        data: {
+                                rating: {
+                                        notes: this.state.rating.notes
+                                }
+                        }
+                }).success(function (response) {
+                        this.setState({ rating: response, editing: false });
+                }.bind(this));
+        },
+        handleNotesChange: function(event) {
+                let rating = this.state.rating;
+                rating.notes = event.target.value;
+                this.setState({rating});
+        },
+        setEditing: function(editing) {
+                this.setState({editing});
+        },
         render: function() {
-                return (
-                          <div className="card grey lighten-5">
-                          <div className="card-content">
-                          <span className="card-title black-text">Feedback from <TimeField time={this.props.rating.created_at}/></span>
-                          <Markdown text={this.props.rating.notes} />
-                          </div>
-                          <div className="card-action">
-                          <a href="#">Edit</a>
-                          </div>
-                          </div>
+                return(
+                                <div className="card grey lighten-5">
+                                <div className="card-content">
+                                <span className="card-title black-text">Feedback from <TimeField time={this.props.rating.created_at}/></span>
+                                {this.ratingBody()}
+                                </div>
+                                <div className="card-action">
+                                {this.renderActions()}
+                                </div>
+                                </div>
 
-                )
+                      )
+        },
+        renderActions: function () {
+                if (this.state.editing) {
+                        return (
+                                        <div>
+                                        <a onClick={this.setEditing.bind(this, false)}>Cancel</a>
+                                        <a onClick={this.update}>Save</a>
+                                        </div>
+                               );
+                } else {
+                        return (<a onClick={this.setEditing.bind(this, true)}>Edit</a>);
+                }
+        },
+        ratingBody: function() {
+                if (this.state.editing) {
+                        return (<div className='input-field row'>
+                                        <textarea ref='notes' name="notes" className="materialize-textarea" value={this.state.rating.notes} onChange={this.handleNotesChange} />
+                                        <label htmlFor="notes">Notes</label>
+                                        </div>);
+                } else {
+                        return (<Markdown text={this.state.rating.notes} />)
+                }
         }
 });
