6416cf2a1fc574347d642c4dc64754fef03108a3
Kurt Nelson <kurtisnelson@gmail.com>
2015-10-22 11:14:02 -0500

    Fix assignment collapse

--- app/assets/javascripts/components/assignment.js.jsx
+++ app/assets/javascripts/components/assignment.js.jsx
@@ -16,42 +16,42 @@ var Assignment = React.createClass({
         },
         render: function() {
                 let submitPath = "/submissions/new?assignment=" + this.props.id
-                        let assignmentPath = "/assignments/" + this.props.id
-                        return (<li className="full-width" ref={(ref) => this.listItem = ref}>
-                                        <div className="collapsible-header">
-                                        {this.renderFeedbackIcon()}
-                                        {this.renderIcon()}
-                                        <a href={assignmentPath}>{this.state.assignment.title}</a> due <strong><TimeField hoverable time={this.state.assignment.due_date}/></strong>
-                                        </div>
-                                        <div className="collapsible-body white">
-                                        {this.renderSubmissions()}
-                                        <p>
+                let assignmentPath = "/assignments/" + this.props.id
+                return (<li className="full-width">
+                        <div className="collapsible-header">
+                                {this.renderFeedbackIcon()}
+                                {this.renderIcon()}
+                                <a href={assignmentPath}>{this.state.assignment.title}</a> due <strong><TimeField hoverable time={this.state.assignment.due_date}/></strong>
+                        </div>
+                        <div className="collapsible-body white">
+                                {this.renderSubmissions()}
+                                <p>
                                         <a href={submitPath} className="btn">Submit<i className="material-icons right">send</i></a>
-                                        </p>
-                                        </div>
-                                        </li>);
+                                </p>
+                        </div>
+                </li>);
         },
         renderSubmissions: function() {
                 if(this.state.submissions) {
                         return (<ul className="collection">
-                                        {this.state.submissions.map(function (submission) {
-                                                                                                  let submissionPath = "/submissions/"+submission.id
-                                                                                                          let submissionIcon = submission.hasFeedback ? "announcement" : "chat_bubble_outline"
-                                                                                                          let submissionTime = "Submitted " + submission.on_time + " at " + submissions.created
-                                                                                                          return (
-                                                                                                                          <li className="collection-item">
-                                                                                                                          <a href={submissionPath}>
-                                                                                                                          <i className="material-icons left">{submissionIcon}</i>
-                                                                                                                          </a>
-                                                                                                                          {submissionTime}
-                                                                                                                          <strong>{submission.label}</strong>
-                                                                                                                          <a href={submissionPath}>
-                                                                                                                          <i className="material-icons right">input</i>
-                                                                                                                          </a>
-                                                                                                                          </li>
-                                                                                                                 );
-                                                                                          })}
-                                        </ul>)
+                                {this.state.submissions.map(function (submission) {
+                                        let submissionPath = "/submissions/"+submission.id
+                                        let submissionIcon = submission.hasFeedback ? "announcement" : "chat_bubble_outline"
+                                        let submissionTime = "Submitted " + submission.on_time + " at " + submissions.created
+                                        return (
+                                                <li className="collection-item">
+                                                        <a href={submissionPath}>
+                                                                <i className="material-icons left">{submissionIcon}</i>
+                                                        </a>
+                                                        {submissionTime}
+                                                        <strong>{submission.label}</strong>
+                                                        <a href={submissionPath}>
+                                                                <i className="material-icons right">input</i>
+                                                        </a>
+                                                </li>
+                                                );
+                                })}
+                        </ul>)
                 } else {
                         return <p>Not submitted yet.</p>
                 }
@@ -60,27 +60,27 @@ var Assignment = React.createClass({
                 let icon;
                 switch(this.state.assignment.status) {
                         case "completed":
-                              icon = "done"
-                                      break
+                                icon = "done"
+                                break
                         case "pending":
-                              icon = "code"
-                                      break
+                                icon = "code"
+                                break
                         case "incomplete":
-                              icon = "incomplete"
-                                      break
+                                icon = "incomplete"
+                                break
                         case "late":
-                              icon = "report_problem"
-                                      break
+                                icon = "report_problem"
+                                break
                         default:
-                              icon = "assignment"
+                                icon = "assignment"
                 }
                 return (<i className="material-icons">{icon}</i>)
         },
         renderFeedbackIcon: function() {
                 let icon = "chat_bubble_outline"
-                        if(this.state.assignment.has_feedback) {
-                                icon = "announcement"
-                        }
+                if(this.state.assignment.has_feedback) {
+                        icon = "announcement"
+                }
                 return (<i className="material-icons">{icon}</i>)
         },
         parse: function(response) {
--- app/assets/javascripts/components/my_assignments.js.jsx
+++ app/assets/javascripts/components/my_assignments.js.jsx
@@ -3,58 +3,59 @@ var MyAssignments = React.createClass({
                 return { assignments: [] }
         },
         componentWillMount: function() {
-          jQuery.ajax({
-            url: "/assignments",
-            dataType: 'json',
-            success: function(response) {
-                    this.parse(response);
-            }.bind(this),
-            error: function(xhr, status, err) {
-                    console.error(status, err.toString());
-            }.bind(this)
-          });
+                jQuery.ajax({
+                        url: "/assignments",
+                        dataType: 'json',
+                        success: function(response) {
+                                this.parse(response);
+                                jQuery(this.refs.assignments).collapsible();
+                        }.bind(this),
+                        error: function(xhr, status, err) {
+                                console.error(status, err.toString());
+                        }.bind(this)
+                });
         },
         parse: function(response) {
                 this.setState({assignments: response.data});
         },
         render: function() {
                 return (
-                                <div>
+                        <div>
                                 <SearchAssignments />
-                                <ul className="collapsible black-text popout">
-                                <li className="full-width">
-                                        <div className="collapsible-header">
-                                                <i className="material-icons left">language</i>
-                                                Icon Reference
-                                        </div>
-                                        <div className="collapsible-body white">
-                                                <ul className="collection">
-                                                {this.renderKey()}
-                                                </ul>
-                                        </div>
-                                </li>
-                                {this.state.assignments.map(function(assignment) {
-                                                                                         return (
-                                                                                                         <Assignment key={assignment.id} id={assignment.id} />
-                                                                                                )
-                                                                                 })
-                                }
+                                <ul className="collapsible black-text popout" ref="assignments">
+                                        <li className="full-width">
+                                                <div className="collapsible-header">
+                                                        <i className="material-icons left">language</i>
+                                                        Icon Reference
+                                                </div>
+                                                <div className="collapsible-body white">
+                                                        <ul className="collection">
+                                                                {this.renderKey()}
+                                                        </ul>
+                                                </div>
+                                        </li>
+                                        {this.state.assignments.map(function(assignment) {
+                                                return (
+                                                        <Assignment key={assignment.id} id={assignment.id} />
+                                                        )
+                                        })
+                                        }
                                 </ul>
-                                </div>)
+                        </div>)
         },
         renderKey: function() {
                 let icons = [['announcement', 'Instructor or TA has left feedback on this submission.'],
-                ['chat_bubble_outline', 'No feedback has been left on this assignment.'],
-                ['done', 'This assignment is completed. Good job!'],
-                ['code', 'This assignment is currently in code review.'],
-                ['not_interested', 'This assignment was marked as incomplete.'],
-                ['report_problem', 'This assignment has no submissions and is now late.'],
-                ['assignment', 'This is an assignment. You probably have several of these.']]
-                return icons.map(function(icon) {
-                        return (<li key={icon[0]} className="collection-item">
+                        ['chat_bubble_outline', 'No feedback has been left on this assignment.'],
+                        ['done', 'This assignment is completed. Good job!'],
+                        ['code', 'This assignment is currently in code review.'],
+                        ['not_interested', 'This assignment was marked as incomplete.'],
+                        ['report_problem', 'This assignment has no submissions and is now late.'],
+                        ['assignment', 'This is an assignment. You probably have several of these.']]
+                        return icons.map(function(icon) {
+                                return (<li key={icon[0]} className="collection-item">
                                         <i className="material-icons left">{icon[0]}</i>
                                         {icon[1]}
-                                        </li>);
-                })
-      }
+                                </li>);
+                        })
+        }
 });
