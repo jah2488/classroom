d97789467448a18167aaaea40cfa01a190bd2be0
Kurt Nelson <kurtisnelson@gmail.com>
2015-11-05 11:04:51 -0600

    Checkin controller: unhappy specs

--- spec/controllers/checkins_controller_spec.rb
+++ spec/controllers/checkins_controller_spec.rb
@@ -23,5 +23,26 @@ RSpec.describe CheckinsController do
         expect(response).to have_http_status(:success)
       end
     end
+
+    it "fails if student is already checked in" do
+      time = cohort.start_time
+      Timecop.freeze(time.to_datetime) do
+        sign_in student_user
+        create :day, cohort: cohort, start: time
+        post :create, distance: 0
+      end
+      post :create, distance: 0
+      expect(response).to have_http_status(:bad_request)
+    end
+
+    it "rejects off-campus checkin without override code" do
+      time = cohort.start_time
+      Timecop.freeze(time.to_datetime) do
+        sign_in student_user
+        create :day, cohort: cohort, start: time
+        post :create, distance: 5
+      end
+      expect(response).to have_http_status(:unauthorized)
+    end
   end
 end
