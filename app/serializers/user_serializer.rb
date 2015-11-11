9c9ae86c03f258efd3d6d8349a16525e3cb5785b
Kurt Nelson <kurtisnelson@gmail.com>
2015-11-10 19:09:48 -0600

    Expose avatar_url

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,8 +2,13 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :name, :github
 
   attributes :student?, :instructor?, :operator?
+  attributes :avatar_url
 
   has_one :student
   has_one :instructor
   has_one :operator
+
+  def avatar_url
+    object.gravatar_url(default: :identicon, size: 80, secure: true)
+  end
 end
